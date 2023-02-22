9e7dda5175122e3ffc31b4473e78b39a9bb71032
juan Ignacio Colli <Juan Ignacio Colli>
2023-02-22 17:49:42 -0300

    ChatDaoFactory agregado

---
+++ modules/chat/persistence/ChatDaoFactory.js
@@ -0,0 +1,13 @@
+const { ChatDaoMongoAtlas } = require('./daos/ChatDaoMongoAtlas')
+
+class ChatDaoFactory {
+    getDao(type) {
+        if (type === 'production') { return ChatDaoMongoAtlas.getInstance() }
+        else if (type === 'development') { return ChatDaoMongoDB.getInstance() }
+        else { console.error('error connecting to Dao') }
+    }
+}
+
+module.exports = {
+    ChatDaoFactory
+}
